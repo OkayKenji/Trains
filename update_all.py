297d8658ec87946939fc078cc7f9a645a5b84daf
Kenji <okurakeng@gmail.com>
2024-12-27 06:42:41 -0500

    clean up via python

--- update_all.py
+++ update_all.py
@@ -1,6 +1,7 @@
 import threading
 import subprocess
-
+import os
+import glob
 def action(ele):
     subprocess.run(['python', f'./{ele}/download_unzip_clean.py'])
     print(f'Done: {ele}')
@@ -17,5 +18,33 @@ def main():
 
     for t in threads:
         t.join()
+
+
+    # List of patterns to match the files you want to delete
+    patterns = [
+        '*/shapes.txt',
+        '*/agency.txt',
+        '*/fare*.txt',
+        '*/feed_*.txt',
+        '*/facilities*.txt',
+        '*/levels*.txt',
+        '*/lines*.txt',
+        '*/linked*.txt',
+        '*/multi*.txt',
+        '*/pathwa*.txt',
+        '*/route_p*.txt',
+        '*/stop_are*.txt',
+        '*/timeframes*.txt',
+        '*/areas.txt'
+    ]
+
+    # Loop through each pattern and delete matching files
+    for pattern in patterns:
+        for file_path in glob.glob(pattern):
+            try:
+                os.remove(file_path)
+                print(f"Deleted: {file_path}")
+            except Exception as e:
+                print(f"Error deleting {file_path}: {e}")
 
 main()
